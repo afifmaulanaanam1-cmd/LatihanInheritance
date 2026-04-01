08dbe40efd5b97161ec46f63fc6a5ec0f765d6a1
afifmaulanaanam1-cmd <afifmaulanaanam1@gmail.com>
2026-04-01 14:48:10 +0700

    membuat class induk user & class turunan siswa guru

--- Program.cs
+++ Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Siswa siswaObj = new Siswa("Aji", "Aji@gmial.com", "110510", "Siswa", "XPG6");
+Guru guruObj = new Guru("Bu Ida", "IdaMustika@gmail.com", "990990", "Guru", "MTK");
+
+siswaObj.TampilkanInfo();
+siswaObj.SubmitTugas();
+
+Console.WriteLine();
+
+guruObj.TampilkanInfo();
+guruObj.NilaiTugas();
+class User
+{
+    public string Nama;
+    public string Email;
+    public string Password;
+    public string Role;
+
+    public User(string nama, string email, string password, string role)
+    {
+        Nama = nama;
+        Email = email;
+        Password = password;
+        Role = role;
+    }
+
+    public void TampilkanInfo()
+    {
+        Console.WriteLine("Menampilkan Info User.....");
+        Console.WriteLine($"Nama : {Nama}");
+        Console.WriteLine($"Role : {Role}");
+    }
+}
+
+class Siswa : User 
+{
+    public string ClassName;
+
+    public Siswa(string nama, string email, string password, string role, string classname) : base(nama, email, password, role)
+    {
+        ClassName = classname;
+    }
+
+    public void SubmitTugas()
+    {
+        Console.WriteLine($"Siswa {Nama} sudah mengumpulkan semua tugas");
+    }
+}
+
+class Guru : User
+{
+    public string Mapel;
+    public Guru(string nama, string email, string password, string role, string mapel) : base(nama, email, password, role)
+    {
+        Mapel = mapel;
+    }
+
+    public void NilaiTugas()
+    {
+        Console.WriteLine($"Guru {Nama} sudah menilai semua tugas {Mapel}");
+    }
+}
